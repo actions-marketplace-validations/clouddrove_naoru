6ab1ac178a713bb08f63c07e8e8f6a92d4fa371f
Anmol Nagpal <ianmolnagpal@gmail.com>
2026-06-12 06:30:48 -0400

    chore: add minimal eslint flat config so lint passes

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+import globals from 'globals'
+
+export default [
+  {
+    files: ['src/**/*.js', 'test/**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {},
+  },
+]
